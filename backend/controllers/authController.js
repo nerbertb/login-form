2bf49f407bccd7a9514aa140dc8645aedd493abb
nerbertb <nerbertb@gmail.com>
2025-11-25 23:31:48 +0800

    fix: able to test POST login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,17 +1,17 @@
 export const loginController = (req, res) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    if (email !== 'admin@test.com' || password !== '1234') {
-        return res.status(400).json({
-            error: {
-                message: 'Invalid Credentials',
-            }
-        })
-    };
-
-    res.status(200).json({
-        success: {
-            message: 'Login Successfully'
-        }
+  if (email !== 'admin@test.com' || password !== '1234') {
+    return res.status(400).json({
+      error: {
+        message: 'Invalid Credentials',
+      },
     });
-}
+  }
+
+  res.status(200).json({
+    success: {
+      message: 'Login Successfully',
+    },
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 
+import authRoutes from './routes/authRoutes.js';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to your server.');
 })
+
+
 app.listen(PORT, () => (console.log(`Listening to port ${PORT}`)));
