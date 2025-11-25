1f7c49a619c62837cda5b4a6da08129157ece4f5
nerbertb <nerbertb@gmail.com>
2025-11-25 22:40:15 +0800

    fix: controller logic

---
+++ backend/controllers/authController.js
@@ -0,0 +1,17 @@
+export const loginController = (req, res) => {
+    const {email, password} = req.body;
+
+    if (email !== 'admin@test.com' || password !== '1234') {
+        return res.status(400).json({
+            error: {
+                message: 'Invalid Credentials',
+            }
+        })
+    };
+
+    res.status(200).json({
+        success: {
+            message: 'Login Successfully'
+        }
+    });
+}
