4591d746aaa4a45ae87c4f3dba042f7dc6c9686a
nerbertb <nerbertb@gmail.com>
2025-11-25 20:19:21 +0800

    feat: backend and frontend initial code

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to your server.');
+})
+app.listen(PORT, () => (console.log(`Listening to port ${PORT}`)));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <section>
+      <form method="post">
+        <label>
+          <input type="text" placeholder='User'/>
+        </label>
+        <br />
+        <label>
+          <input type="password" placeholder='Password'/>
+        </label>
+      </form>
+    </section>
+  );
+}
+
+export default App;
